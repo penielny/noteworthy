a3707088b88cf43a9e961ae843806c16b9d16f9e
penielny <penielnyinaku@gmail.com>
2025-06-23 10:15:14 +0000

    feat: add routing and components for notes application
    - Implement routing for notes application with paths for notes list, individual note, archived notes, and create note.
    - Create ArchivedComponent with basic template and styles.
    - Creat CreateNoteComponent with basic template and styles.
    - Create NoteComponent with basic template and styles.
    - Create NotesComponent with basic template and styles.
    - Add unit tests for all new components.

---
+++ src/app/pages/create-note/create-note.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-note',
+  imports: [],
+  templateUrl: './create-note.component.html',
+  styleUrl: './create-note.component.scss'
+})
+export class CreateNoteComponent {
+
+}
